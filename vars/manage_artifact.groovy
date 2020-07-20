da07a38f708db20bbc8dd3b8ccd95976f005f7b1
trickste <nishantprakashsaini285@gmail.com>
2020-07-20 15:49:33 +0530

    upload

--- vars/manage_artifact.groovy
+++ vars/manage_artifact.groovy
@@ -19,9 +19,28 @@ def artifact_pull(String application_name, String bucket_name, String DEVELOPERE
     try
     {
         echo "Show options new artifact to s3"
-        def command = $/echo ""/$
-        def artifact_name_list = sh (script:""" aws s3 ls s3://${bucket_name}/${application_name} && awk '{printf "%s",$2}' && sed 's|/|,|g'""", returnStdout: true).trim()
-        input message: '', parameters: [extendedChoice(description: '', multiSelectDelimiter: ',', name: '', quoteValue: false, saveJSONParameterToFile: false, type: 'PT_SINGLE_SELECT', value: """${artifact_name_list}""", visibleItemCount: 5)]
+        node {
+    stage('Preparation') {
+     input message: '', parameters: [extendedChoice(bindings: '', defaultValue: '', description: '', descriptionPropertyValue: '', groovyClasspath: '', groovyScript: '''def sout = new StringBuilder(), serr = new StringBuilder()
+def proc = \'aws s3 ls s3://${bucket_name}/${application_name}/\'.execute()
+proc.consumeProcessOutput(sout, serr)
+proc.waitForOrKill(2000)
+def values = "$sout".split(\'/\')
+def trimmedValues
+def parameters=[]
+values.each {  println "${it}" }
+def cleanValues = "$sout".split(\'PRE\')
+def last = cleanValues.last().split(\'2018-12-17\')[0]
+cleanValues.each {  "${it}".toString(); 
+                    trimmedValues = "${it}".trim();
+                    parameters<<trimmedValues
+                 }
+parameters.remove(parameters.size() - 1);
+parameters.add(last)
+parameters''', multiSelectDelimiter: ',', name: '', quoteValue: false, saveJSONParameterToFile: false, type: 'PT_SINGLE_SELECT', visibleItemCount: 5)]
+    }
+}
+
         sh """sudo rm -r ${application_name}_src/*"""
         sh """aws s3 sync s3://${bucket_name}/${application_name}/${artifact_name} ${application_name}_src"""
     }
